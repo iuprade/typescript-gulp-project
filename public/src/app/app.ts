10ba023cb67d09b27ffc461a8f7b4d8928d4f977
Indra Kumar Uprade <indrakumar.uprade@synchronoss.com>
2016-10-04 11:56:27 -0400

    initial commit

---
+++ public/src/app/app.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../../node_modules/gulp-typescript/release/main.d.ts" />
+
+angular.module('sampleTypeScriptProjectAngular2App', [
+    'app.demo',
+    'app.templates'
+]);
+
+// your app setup here
